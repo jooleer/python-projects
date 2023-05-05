e0d6123f5fd725f5c61019c15104e11c48254833
jooleer <joost@leerd.am>
2023-05-06 00:00:32 +0700

    multithreading & folder hash function

--- hash_compare/newfolderhash.py
+++ hash_compare/newfolderhash.py
@@ -1,3 +1,5 @@
+# this code is still WIP
+
 import os
 import sys
 import hashlib
@@ -5,10 +7,12 @@ import subprocess
 import zlib
 import logging
 import time
+import threading
 
 # define the paths of the two network folders to compare
 folder1_path = r""
 folder2_path = r""
+
 
 # hash algorythm (CRC32, MD5, SHA256)
 hash_algorithm = "CRC32"
@@ -46,8 +50,8 @@ def generate_file_hash(file_path, hash_algorithm="CRC32"):
     with open(file_path, "rb") as f:
         global files
         files += 1
-        sys.stdout.write("Processing file %d of %d (%d%%)\r\n\033[K" % (files, files_amount, (files/files_amount)*100) )
-        sys.stdout.write("Generating hash for file: %s\r\033[F" % (file_path) )
+        # sys.stdout.write("Processing file %d of %d (%d%%)\r\n\033[K" % (files, files_amount, (files/files_amount)*100) )
+        # sys.stdout.write("Generating hash for file: %s\r\033[F" % (file_path) )
         sys.stdout.flush()
         file_data = f.read()
         if hash_algorithm == "CRC32":
@@ -72,22 +76,40 @@ def get_all_files(folder_path):
     return all_files
 
 
-# generate hash values for each file in folder 1
+def folder_generate_hashes(folder_path):
+    # generate hash values for each file in folder
+    folder_hashes = {}
+    for file_path in get_all_files(folder_path):
+        file_hash = generate_file_hash(file_path, hash_algorithm)
+        relative_path = os.path.relpath(file_path, folder_path)
+        folder_hashes[relative_path] = file_hash
+        # print(f"Done: {relative_path}")
+    return folder_hashes
+
+
 folder1_hashes = {}
-for file_path in get_all_files(folder1_path):
-    file_hash = generate_file_hash(file_path, hash_algorithm)
-    relative_path = os.path.relpath(file_path, folder1_path)
-    folder1_hashes[relative_path] = file_hash
-
-# generate hash values for each file in folder 2
 folder2_hashes = {}
-for file_path in get_all_files(folder2_path):
-    file_hash = generate_file_hash(file_path, hash_algorithm)
-    relative_path = os.path.relpath(file_path, folder2_path)
-    folder2_hashes[relative_path] = file_hash
+
+def gen_folder1_hashes(folder_path=folder1_path):
+    global folder1_hashes
+    folder1_hashes = folder_generate_hashes(folder_path)
+    
+def gen_folder2_hashes(folder_path=folder2_path):
+    global folder2_hashes
+    folder2_hashes = folder_generate_hashes(folder_path)
+    
+
+t1 = threading.Thread(target=gen_folder1_hashes, args=(folder1_path,))
+t2 = threading.Thread(target=gen_folder2_hashes, args=(folder2_path,))
+
+t1.start()
+t2.start()
+
+t1.join()
+t2.join()
 
 
-
+''' 
 # check for missing files in folder 1
 for file_path in get_all_files(folder2_path):
     relative_path = os.path.relpath(file_path, folder2_path)
@@ -103,7 +125,7 @@ for file_path in get_all_files(folder1_path):
         # print(bcolors.WARNING + f"{relative_path} is missing from {folder2_path}." + bcolors.ENDC)
         logging.info(f"[WARNING - MISSING FILE]: {relative_path}")
         files_missing += 1
-
+'''
 
 # compare the hash values for each file in both folders
 for relative_path in set(folder1_hashes.keys()).intersection(set(folder2_hashes.keys())):
